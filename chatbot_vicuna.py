0229eae8548469abe330bec06be4961c486d4e07
Huy-Vu-Quang <vujohnny219@gmail.com>
2025-10-08 04:06:14 +0700

    reset

--- chatbot_vicuna.py
+++ chatbot_vicuna.py
@@ -32,14 +32,11 @@ def load_embeddings():
 def load_llm():
     MODEL_NAME = "lmsys/vicuna-7b-v1.5"
 
-    #bnb_config = BitsAndBytesConfig(
-    #    load_in_4bit=True,  # Hoặc load_in_8bit=True
-    #    bnb_4bit_use_double_quant=True,
-    #    bnb_4bit_compute_dtype=torch.bfloat16,
-    #    bnb_4bit_quant_type="nf4"
-    #)
     bnb_config = BitsAndBytesConfig(
-        load_in_8bit=True
+        load_in_4bit=True,  # Hoặc load_in_8bit=True
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_quant_type="nf4"
     )
 
     # Load model với quantization
